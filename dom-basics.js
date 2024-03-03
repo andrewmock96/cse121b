1d588f40fbcc1c1c2e29fc3336b4a43e4e555ba8
andrewmock96 <moc21003@byui.edu>
2024-03-03 12:03:08 -0600

    d

--- dom-basics.js
+++ dom-basics.js
@@ -5,3 +5,9 @@ const newImage = document.createElement("img");
 newImage.setAttribute("src", "https://picsum.photos/200");
 newImage.setAttribute("alt", "Description of image");
 document.body.appendChild(newImage);
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+const newSection = document.createElement("section")
+newSection.innerHTML = "<section><h2>CSE 121b</h2><p>Welcome to Javascript Language</p></section>"
+document.body.appendChild(newSection)
