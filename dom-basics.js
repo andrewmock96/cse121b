3fba7d36f5f0c99680355a4fa795b676e0ea0b4f
andrewmock96 <moc21003@byui.edu>
2024-03-03 11:57:12 -0600

    s

--- dom-basics.js
+++ dom-basics.js
@@ -1,3 +1,7 @@
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Description of image");
+document.body.appendChild(newImage);
