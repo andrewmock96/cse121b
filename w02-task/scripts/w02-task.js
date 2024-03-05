120eb85ad6a4631238dd7f6c601314113508926a
andrewmock96 <moc21003@byui.edu>
2024-03-04 21:32:52 -0600

    d

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,39 +3,35 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = "Andrew Mock";
-let currentYear = "2024";
-let profilePicture = "images/profile.png";
+const fullName = "Andrew Mock";
+const currentYear = "2024";
+const profilePicture = "images/profile.png";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-const imageElement = document.getElementById("home");
+const imageElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
-imageElement.src = profilePicture;
-imageElement.alt = `Profile image of ${fullName}`;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 
-let favFoods = ["Pizza", "Chicken Alfredo", "Ice Cream", "Pie", "Pulled Pork", "Brisket"]
+let favFoods = ["Pizza", "Chicken Alfredo", "Ice Cream", "Pie", "Pulled Pork", "Brisket"];
 
 function formatArrayWithCommas(array) {
   return array.join(", ");
 }
 
-foodElement.innerHTML = formatArrayWithCommas(favFoods) + "<br><br>";
+foodElement.innerHTML = formatArrayWithCommas(favFoods) + "<br>";
 let newFavoriteFood = "Sushi";
 favFoods.push(newFavoriteFood);
-foodElement.innerHTML += formatArrayWithCommas(favFoods) + "<br><br>";
+foodElement.innerHTML += formatArrayWithCommas(favFoods) + "<br>";
 favFoods.shift();
-foodElement.innerHTML += formatArrayWithCommas(favFoods) + "<br><br>";
+foodElement.innerHTML += formatArrayWithCommas(favFoods) + "<br>";
 favFoods.pop();
-foodElement.innerHTML += formatArrayWithCommas(favFoods) + "<br><br>";
-
-
-
-
+foodElement.innerHTML += formatArrayWithCommas(favFoods) + "<br>";
